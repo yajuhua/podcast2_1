c2c7fdbb167256c8cd705ddf8442f6c372bd619f
yajuhua <yajuhua@outlook.com>
2024-11-21 16:01:34 +0800

    [update]配置devServer方便开发

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,15 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-  // devServer: {
-  //   port: 7070,
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:8088',
-  //       pathRewrite: {
-  //         '^/api': ''
-  //       }
-  //     }
-  //   }
-  // }
+  transpileDependencies: true,
+  devServer: {
+    port: 7070,  // 前端开发服务器端口
+    proxy: {
+      '^/ws/download': {
+        target: 'ws://localhost:8088',
+        ws: true,
+        changeOrigin: true
+      },
+      '^/ws/logs': {
+        target: 'ws://localhost:8088',
+        ws: true,
+        changeOrigin: true
+      },
+      '/': {
+        target: 'http://localhost:8088',  // 后端 HTTP 服务地址
+        ws: false,
+        changeOrigin: true
+      }
+    },
+  }
 })
