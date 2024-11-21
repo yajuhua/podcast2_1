4b496b26a8ab2111f02dfc14fa7bc2e2874157e1
yajuhua <yajuhua@outlook.com>
2024-11-21 13:40:42 +0800

    [fix]IP地址范围过大导致爆内存

--- src/main/java/io/github/yajuhua/podcast2/common/utils/NetWorkUtils.java
+++ src/main/java/io/github/yajuhua/podcast2/common/utils/NetWorkUtils.java
@@ -1,20 +1,20 @@
 package io.github.yajuhua.podcast2.common.utils;
 
 import io.github.yajuhua.podcast2.pojo.entity.AddressFilter;
-import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 /**
  * 网络工具类
  */
 public class NetWorkUtils {
+
     /**
      * 验证IP地址范围是否有效
-     *
      * @param ipRange 例IP范围:192.168.1.1-192.168.2.255
-     * @return
+     * @return true or false
      */
     public static boolean isValidIpRange(String ipRange) {
         String[] parts = ipRange.split("-");
@@ -37,27 +37,7 @@ public class NetWorkUtils {
     }
 
     /**
-     * 根据IP范围获取范围内所有IP地址
-     *
-     * @param ipRange 例IP范围:192.168.1.1-192.168.2.255
-     * @return
-     */
-    public static List<String> generateRangeIpList(String ipRange) {
-        String[] parts = ipRange.split("-");
-        String startIp = parts[0];
-        String endIp = parts[1];
-        int startIpAsInt = ipToInt(startIp);
-        int endIpAsInt = ipToInt(endIp);
-        List<String> ipList = new ArrayList<>();
-        for (int i = startIpAsInt; i <= endIpAsInt; i++) {
-            ipList.add(intToIp(i));
-        }
-        return ipList;
-    }
-
-    /**
      * 将IP地址转换为整数（网络字节序）
-     *
      * @param ip 192.168.1.1
      * @return
      */
@@ -71,27 +51,44 @@ public class NetWorkUtils {
     }
 
     /**
-     * 将整数转换为IP地址
-     *
-     * @param ip 192.168.1.1
-     * @return
-     */
-    private static String intToIp(int ip) {
-        return (ip >>> 24) + "." +
-                ((ip >> 16) & 0xFF) + "." +
-                ((ip >> 8) & 0xFF) + "." +
-                (ip & 0xFF);
-    }
-
-    /**
      * 使用正则表达式进行匹配 验证IPv4地址是否有效
-     *
      * @param ip 例IP地址:192.168.1.1
-     * @return
+     * @return true or false
      */
     public static boolean isValidIp(String ip) {
         String pattern = "^((\\d{1,2}|1\\d{2}|2[0-4]\\d|25[0-5])\\.){3}(\\d{1,2}|1\\d{2}|2[0-4]\\d|25[0-5])$";
         return ip.matches(pattern);
+    }
+
+    /**
+     * 将 IP 地址转换为 32 位整数
+     * @param ip IPv4字符串 如 1.1.1.1
+     * @return 32 位整数
+     * @throws UnknownHostException
+     */
+    private static long ipToLong(String ip) throws UnknownHostException {
+        InetAddress inetAddress = InetAddress.getByName(ip);
+        byte[] address = inetAddress.getAddress();
+        long result = 0;
+        for (int i = 0; i < address.length; i++) {
+            result |= ((address[i] & 0xFF) << (24 - (8 * i)));
+        }
+        return result;
+    }
+
+    /**
+     * 判断 IP 是否在指定的范围内
+     * @param ip
+     * @param startIp 开始IP地址
+     * @param endIp 结束IP地址
+     * @return true or false
+     * @throws UnknownHostException
+     */
+    private static boolean isInRange(String ip, String startIp, String endIp) throws UnknownHostException {
+        long ipLong = ipToLong(ip);
+        long startIpLong = ipToLong(startIp);
+        long endIpLong = ipToLong(endIp);
+        return ipLong >= startIpLong && ipLong <= endIpLong;
     }
 
     /**
@@ -100,54 +97,45 @@ public class NetWorkUtils {
      * @param addressFilter
      * @return
      */
-    public static boolean isBan(String remoteAddr, AddressFilter addressFilter){
-        List<String> whitelist = addressFilter.getWhitelist();
-        List<String> blacklist = addressFilter.getBlacklist();
-        List<String> banList = new ArrayList<>();
+    public static boolean isBan(String remoteAddr, AddressFilter addressFilter) {
+        List<String> whiteList = addressFilter.getWhitelist();
+        List<String> blackList = addressFilter.getBlacklist();
 
-        //黑名单
-        for (String b : blacklist) {
-            boolean isIp = isValidIp(b);
-            if (isIp){
-                //单个ip
-                if (b.equals(remoteAddr)){
-                    banList.add(b);
-                }
-            }else {
-                //IP范围
-                List<String> ipList = generateRangeIpList(b);
-                for (String s : ipList) {
-                    if (s.equals(remoteAddr)){
-                        banList.add(s);
+        try {
+            // 判断 IP 是否在白名单中
+            for (String whiteIp : whiteList) {
+                if (whiteIp.contains("-")) { // 处理范围
+                    String[] range = whiteIp.split("-");
+                    if (isInRange(remoteAddr, range[0], range[1])) {
+                        return false; // 白名单中有范围匹配的 IP，不禁止
+                    }
+                } else {
+                    if (remoteAddr.equals(whiteIp)) {
+                        return false; // 白名单中有精确匹配的 IP，不禁止
                     }
                 }
             }
-        }
 
-        //白名单
-        for (String w : whitelist) {
-            boolean isIp = isValidIp(w);
-            if (isIp){
-                //单个ip
-                if (w.equals(remoteAddr)){
-                    banList.remove(w);
-                }
-            }else {
-                //IP范围
-                List<String> ipList = generateRangeIpList(w);
-                for (String s : ipList) {
-                    if (s.equals(remoteAddr)){
-                        banList.remove(s);
+            // 判断 IP 是否在黑名单中
+            for (String blackIp : blackList) {
+                if (blackIp.contains("-")) { // 处理范围
+                    String[] range = blackIp.split("-");
+                    if (isInRange(remoteAddr, range[0], range[1])) {
+                        return true; // 黑名单中有范围匹配的 IP，禁止
+                    }
+                } else {
+                    if (remoteAddr.equals(blackIp)) {
+                        return true; // 黑名单中有精确匹配的 IP，禁止
                     }
                 }
             }
-        }
 
-        //如果banList不等于0。说明被ban了
-        if (banList.isEmpty()){
+            // 默认情况下，如果 IP 不在白名单或黑名单中，允许
             return false;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
         }
-        return true;
+        return false;
     }
 
 }
